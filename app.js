f1925d207abebd587fca1d0cadd39ca5bf609cae
Courtney-Dastrup <courtneymd15.2@gmail.com>
2022-03-16 18:47:55 -0600

    Actual Turn In

---
+++ app.js
@@ -0,0 +1,14 @@
+//3 and 4
+$('.navItem').css("color", "white");
+
+//5 and 6
+$('#nav').css("background-color", "#145");
+
+//7 and 8
+$('li:even').css("background-color", "#ccc");
+
+//9 and 10
+$('li:odd').css("background-color", "#aaa");
+
+//11 and 12
+$(':input').val("Courtney");
